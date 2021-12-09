91872e09484378d02e51b0e8469c70b878ccfbac
Pjose <patrick.jose@fors.unil.ch>
2021-12-09 16:41:59 +0100

    -Setting up ENtriesModule

---
+++ src/app/pages/entires/entires.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EntiresRoutingModule } from './entires-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EntiresRoutingModule
+  ]
+})
+export class EntiresModule { }
